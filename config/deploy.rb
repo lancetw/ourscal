d54e6a3f9a3c57e7a996a22f5bd10db2be380266
Hsin-lin Cheng <lancetw@gmail.com>
2013-06-26 16:30:27 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :branch, "master"
 set :scm_verbose, true
 
 # if you want to clean up old releases on each deploy uncomment this:
-#after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -56,7 +56,7 @@ namespace :deploy do
 end
 
 after "deploy:update_code", "deploy:copy_config_files"
-after "deploy:finalize_update", "deploy:update_symlink"
+#after "deploy:finalize_update", "deploy:update_symlink"
 
 after "bundle:install" do
   run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
