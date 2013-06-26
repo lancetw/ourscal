188bbf4f00de14d580f6a608fb9ba77d33fef957
Hsin-lin Cheng <lancetw@gmail.com>
2013-06-26 16:28:02 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,5 +56,8 @@ namespace :deploy do
 end
 
 after "deploy:update_code", "deploy:copy_config_files"
-# after "deploy:finalize_update", "deploy:update_symlink"
+after "deploy:finalize_update", "deploy:update_symlink"
 
+after "bundle:install" do
+  run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+end
