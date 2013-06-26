3b9111471687f105ab48899b9a8edef2f4ebc499
Hsin-lin Cheng <lancetw@gmail.com>
2013-06-26 16:56:08 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,3 +58,15 @@ end
 after "deploy:update_code", "deploy:copy_config_files"
 #after "deploy:finalize_update", "deploy:update_symlink"
 
+namespace :deploy do
+    namespace :assets do
+      task :precompile, :roles => :web, :except => { :no_release => true } do
+        from = source.next_revision(current_revision)
+        if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+    end
+  end
+end
